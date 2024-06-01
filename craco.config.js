ee8fa7088170c10054037141b7099c68984d4ef4
Fakhrity Hikmawan <fakhrityhikmawan@gmail.com>
2024-06-01 18:09:11 +0700

    Fix bug Sourcemap

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
     },
+    configure: (webpackConfig) => {
+      if (process.env.NODE_ENV === 'production') {
+        webpackConfig.optimization.minimizer.push(
+          new TerserPlugin({
+            terserOptions: {
+              compress: {
+                drop_console: true,
+              },
+            },
+          })
+        );
+      }
+      return webpackConfig;
+    },
+  },
+  babel: {
+    plugins: [
+      process.env.NODE_ENV === 'production' && 'transform-remove-console',
+    ].filter(Boolean),
   },
 };
